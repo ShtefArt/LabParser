69420dd64dfc38154ea3d23e157f97bcb3ba3a97
ShtefArt <shtefanesaart@gmail.com>
2021-11-20 11:43:50 +0200

    lab1

---
+++ main.rb
@@ -0,0 +1,44 @@
+require 'open-uri'
+require 'byebug'
+require 'nokogiri'
+require 'csv'
+require 'json'
+require 'date'
+
+url = 'https://www.ukr.net/'
+html = URI.open(url) { |result| result.read }
+
+document = Nokogiri::HTML(html)
+
+news = 
+  document.css('article section > div').map do |tr_node|
+    {
+      time: tr_node.css('time').text,
+      news: tr_node.css('div > a').text,
+      publisher: tr_node.css('div > span').text
+    }
+  end
+ csv= CSV.open("data.csv", "w") do |wr|
+    #wr << [news.first]
+ end
+ byebug
+news = news.delete_if { |n| n[:time].empty? }
+byebug
+puts JSON.pretty_generate(news)
+
+column_names = news.first.keys
+    s=CSV.generate do |csv|
+    csv << column_names
+    news.each do |x|
+    #csv << x.values
+  end
+end
+File.write('data.csv', s)
+csv= CSV.open("data.csv", "w") do |wr|
+  wr << [news]
+end
+
+#puts JSON.pretty_generate(players)
+
+byebug
+puts news.inspect
